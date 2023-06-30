cc2a422681ce9afefc3911293cfb9fee32b1e1fb
Joaomiguelgcrp <113705589+Joaomiguelgcrp@users.noreply.github.com>
2023-06-30 15:53:18 +0100

    Add files via upload

---
+++ fonts.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(patchwork)
+library(showtext)
+
+
+font_add_google("Montserrat", "Montserrat")
+font_add_google("Ubuntu", "Ubuntu")
+font_add_google("Oswald", "Oswald")
+font_add_google("Rock Salt", "Rock Salt")
+
+showtext_auto()
+
+
+
+plot <- 
+  ggplot(mtcars, aes(x=wt, y=mpg)) + 
+  geom_point() +
+  labs(
+    title = "Fuel Efficiency of 32 Cars",
+    x= "Weight (x1000 lb)",
+    y = "Miles per Gallon"
+  )+
+  theme_minimal()+
+  NULL
+
+p1 <- plot + theme(text=element_text(size=16, family="Montserrat"))
+p2 <- plot + theme(text=element_text(size=16, family="Ubuntu"))
+p3 <- plot + theme(text=element_text(size=16, family="Oswald"))
+p4 <- plot + theme(text=element_text(size=16, family="Rock Salt"))
+
+
+# Use patchwork to arrange  4 graphs on same page
+(p1 + p2) / (p3 + p4)
